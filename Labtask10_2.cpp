f40e818222c58773dbb338689aa0e294e54280e3
blackrose001 <lionwhite835@gmail.com>
2025-11-12 01:26:20 -0800

    Add files via upload

---
+++ Labtask10_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+	cout << "Enter size of array: ";
+	cin >> n;
+	int find[n];
+	cout << "Enter 8 characters: ";
+	for(int i = 0; i < n; i++) {
+		cin >> find[i];
+	}
+	for(int i = 0; i < n; i++) {
+		cout << find[i];
+	}
+}
